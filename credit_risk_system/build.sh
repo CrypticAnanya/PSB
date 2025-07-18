bd4bc0907ee789e5053acb88c6c6e5137f7ffa2c
CrypticAnanya <161688295+CrypticAnanya@users.noreply.github.com>
2025-07-18 14:04:42 +0530

    Update build.sh

--- credit_risk_system/build.sh
+++ credit_risk_system/build.sh
@@ -12,4 +12,8 @@ echo "Populating sample data..."
 python manage.py populate_sample_data
 
 echo "Starting Django server..."
+# Open browser in background
+# xdg-open http://127.0.0.1:8000/ &  # for Linux
+start http://127.0.0.1:8000/     # for Windows (uncomment if using Windows)
+
 python manage.py runserver
